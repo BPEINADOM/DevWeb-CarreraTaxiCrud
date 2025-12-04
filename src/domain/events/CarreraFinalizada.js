bddbb41285ec1673d14f841c3b61704f0f54cccf
BPEINADOM <bpeinadom@unicartagena.edu.co>
2025-12-04 14:50:24 -0500

    Implementacion de los eventos de dominio 'CarreraIniciada' 'CarreraFinalizada'

---
+++ src/domain/events/CarreraFinalizada.js
@@ -0,0 +1,11 @@
+class CarreraFinalizada {
+
+    constructor(idCarrera, fechaHora, precioFinal) {
+        this.idCarrera = idCarrera;
+        this.fechaHora = fechaHora;
+        this.precioFinal = precioFinal;
+    }
+    
+}
+
+module.exports = CarreraFinalizada;
---
+++ src/domain/events/CarreraIniciada.js
@@ -0,0 +1,10 @@
+class CarreraIniciada {
+
+    constructor(idCarrera, fechaHora) {
+        this.idCarrera = idCarrera;
+        this.fechaHora = fechaHora;
+    }
+    
+}
+
+module.exports = CarreraIniciada;
